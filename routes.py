a625d4100857edaefcf687a83bb4d23b3bd885df
Janusz <janusz@nsa.org>
2018-06-15 08:13:02 +0200

    Adding to DB (not sure if working)

--- routes.py
+++ routes.py
@@ -1,6 +1,8 @@
 from server import app, db
 from flask import render_template, request
+from models import User, Orders
 import spur
+import datetime
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -11,6 +13,9 @@ def index():
     val3 = request.form['alfabet[]'] #alphabet
     val4 = request.form['wezly'] #number of clusters
 
+    query = Orders(1, str(datetime.datetime.now()), val1, val4, val3, val2, 0, null, null)
+    db.session.add(query)
+    db.session.commit()
 
     output = "Test"
     # actually there are 3 lines of output: hash, password and time
